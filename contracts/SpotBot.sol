f5c09718a1bade82db392db45760d02951670124
PatoVerde <leandrodenos@gmail.com>
2022-10-29 02:00:57 -0300

    fix fucking abstract error...

--- contracts/SpotBot.sol
+++ contracts/SpotBot.sol
@@ -15,13 +15,14 @@ import "./utils/Swap.sol";
 */
 
 contract SpotBot is AccessControl, Swap {
+
   struct userData{
     uint buyPrice; //In 8 decimals! For chainlink datafeed
     uint sellPrice; //In 8 decimals!
     uint initialAmount;
     address admin;
   }
- 
+  bool paused;
 
   bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
@@ -30,7 +31,7 @@ contract SpotBot is AccessControl, Swap {
   AggregatorV3Interface dataFeed;
   userData data;
 
-
+  //swap router 0xE592427A0AEce92De3Edee1F18E0157C05861564
   constructor(
     address stableCoin_, 
     address tradeableToken_, 
@@ -38,8 +39,9 @@ contract SpotBot is AccessControl, Swap {
     uint buyPrice_,
     uint sellPrice_,
     uint initialAmount_,
-    address ownerOfBot_
-  ) {
+    address ownerOfBot_,
+    address _swapRouter
+  ) Swap(_swapRouter){
     stableCoin = IERC20(stableCoin_);
     tradeableToken = IERC20(tradeableToken_);
     dataFeed = AggregatorV3Interface(dataFeed_);
@@ -49,8 +51,8 @@ contract SpotBot is AccessControl, Swap {
     _grantRole(ADMIN_ROLE, msg.sender);
   }
 
-  //Public functions
-  function buy(address _ownerGrid) public {
+  //------------------ PUBLIC FUNCTIONS ------------------
+  function buy(address _ownerGrid) public onlyRole(ADMIN_ROLE){
     uint balance = getBalanceStable();
     require(balance > 0, "Next movement is sell, dont but now");
     bool canExec = _getPrice() <= getBuyPrice();
@@ -58,7 +60,7 @@ contract SpotBot is AccessControl, Swap {
 
     swapExactInputSingle(balance, address(this), address(stableCoin), address(tradeableToken));
   }
-  function sell(address _ownerGrid) public {
+  function sell(address _ownerGrid) public onlyRole(ADMIN_ROLE){
     uint balance = getBalanceStable();
     require(balance <= 0, "Next movement is sell, dont but now");
     bool canExec = _getPrice() >= getBuyPrice();
@@ -67,7 +69,23 @@ contract SpotBot is AccessControl, Swap {
     swapExactInputSingle(balance, address(this), address(tradeableToken), address(stableCoin));
   }
 
-  // # PUBLIC VIEW FUNCTIONS
+  function withdraw() public onlyRole(ADMIN_ROLE){
+    uint balanceStable = getBalanceStable();
+    uint balanceTradeableToken = getBalanceTradeableToken();
+
+    if(balanceStable>0){
+      stableCoin.transfer(data.admin, balanceStable);
+    }
+    if(balanceTradeableToken>0){
+      tradeableToken.transfer(data.admin, balanceTradeableToken);
+    }
+  }
+  
+  function togglePause() public onlyRole(ADMIN_ROLE){
+    paused = !paused;
+  }
+
+  // ------------------ VIEW FUNCTIONS ------------------
   function getBuyPrice()public view returns(uint){
     return data.buyPrice;
   }
@@ -76,9 +94,12 @@ contract SpotBot is AccessControl, Swap {
   function getBalanceStable() public view returns(uint){
     return stableCoin.balanceOf(address(this));
   }
+  function getBalanceTradeableToken() public view returns(uint){
+    return tradeableToken.balanceOf(address(this));
+  }
 
 
-  // PRIVATE FUNCTIONS -> public for tests
+  // ------------------ PRIVATE FUNCTIONS ------------------ -> public for tests
   function _getPrice() public view returns (uint){
     (,int price,,,) = dataFeed.latestRoundData();
     return uint(price);
