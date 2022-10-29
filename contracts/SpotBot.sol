6375393187342303d44ff740ebd9f82ed65866ad
PatoVerde <leandrodenos@gmail.com>
2022-10-29 01:29:42 -0300

    add Buy and Sell functions, and swap file

--- contracts/SpotBot.sol
+++ contracts/SpotBot.sol
@@ -5,19 +5,28 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./utils/Swap.sol";
 
+/*
+*  Dev @LeanLabiano (youtube chanel ;)
+*  First objetive: 
+    ALL IN automte trading bot.
+*  Two points : BuyPoint (uses all usdc in this contract) and SellPoint(uses all asset in this contract)
+*/
 
-contract SpotBot is AccessControl {
+contract SpotBot is AccessControl, Swap {
   struct userData{
-    uint buyPrice;
-    uint sellPrice;
+    uint buyPrice; //In 8 decimals! For chainlink datafeed
+    uint sellPrice; //In 8 decimals!
+    uint initialAmount;
     address admin;
   }
+ 
 
   bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
-  address stableCoin; // Need One Stable!
-  address tradeableToken;
+  IERC20 stableCoin; // Need One Stable!
+  IERC20 tradeableToken;
   AggregatorV3Interface dataFeed;
   userData data;
 
@@ -28,27 +37,51 @@ contract SpotBot is AccessControl {
     address dataFeed_,
     uint buyPrice_,
     uint sellPrice_,
+    uint initialAmount_,
     address ownerOfBot_
   ) {
-    stableCoin = stableCoin_;
-    tradeableToken = tradeableToken_;
+    stableCoin = IERC20(stableCoin_);
+    tradeableToken = IERC20(tradeableToken_);
     dataFeed = AggregatorV3Interface(dataFeed_);
-    data = userData(buyPrice_, sellPrice_, ownerOfBot_);
+    data = userData(buyPrice_, sellPrice_, initialAmount_, ownerOfBot_);
 
     _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     _grantRole(ADMIN_ROLE, msg.sender);
   }
 
   //Public functions
-  
+  function buy(address _ownerGrid) public {
+    uint balance = getBalanceStable();
+    require(balance > 0, "Next movement is sell, dont but now");
+    bool canExec = _getPrice() <= getBuyPrice();
+    require(canExec, "Over price to buy");
+
+    swapExactInputSingle(balance, address(this), address(stableCoin), address(tradeableToken));
+  }
+  function sell(address _ownerGrid) public {
+    uint balance = getBalanceStable();
+    require(balance <= 0, "Next movement is sell, dont but now");
+    bool canExec = _getPrice() >= getBuyPrice();
+    require(canExec, "Under price to sell");
+
+    swapExactInputSingle(balance, address(this), address(tradeableToken), address(stableCoin));
+  }
+
+  // # PUBLIC VIEW FUNCTIONS
+  function getBuyPrice()public view returns(uint){
+    return data.buyPrice;
+  }
+
+  //if return true => waitingBuy |  if return false => waitingSell
+  function getBalanceStable() public view returns(uint){
+    return stableCoin.balanceOf(address(this));
+  }
 
 
-
-
-  // PRIVATE FUNCTIONS
-  function _getLatestPrice() internal view returns (int){
+  // PRIVATE FUNCTIONS -> public for tests
+  function _getPrice() public view returns (uint){
     (,int price,,,) = dataFeed.latestRoundData();
-    return price;
+    return uint(price);
   }
 
 }
